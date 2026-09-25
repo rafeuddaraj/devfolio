bc0368062bcc15a94463cea6f7b17c5fa848f836
Rafe Uddaraj <learnwithsumit.lws@gmail.com>
2026-09-26 04:21:06 +0600

    DevFolio: pages, components and layout

---
+++ src/data/site.ts
@@ -0,0 +1,11 @@
+export const site = {
+  name: 'Sumit Saha',
+  role: 'Web Developer',
+  bio: 'আমি web নিয়ে কাজ করি আর যা শিখি, সেটা নিয়ে লিখি।',
+  openToWork: true,
+  socials: [
+    { label: 'GitHub', url: 'https://github.com/sumitsaha' },
+    { label: 'YouTube', url: 'https://youtube.com/@LearnwithSumit' },
+    { label: 'LinkedIn', url: 'https://linkedin.com/in/sumitsaha' },
+  ],
+};
